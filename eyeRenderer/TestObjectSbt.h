a1374f4f9ff4b659e26e384e295adfb1e955b558
Blayze Millward <blayze.millward@googlemail.com>
2020-09-14 00:24:21 +0100

    Final commit, just a bit of a tidy up.

--- eyeRenderer/TestObjectSbt.h
+++
@@ -1,9 +0,0 @@
-// A header file to contain the SBT data definitions for the test object
-#include "SbtRecord.h"
-
-struct TestObjectData
-{
-  float r, g, b;
-};
-
-typedef SbtRecord<TestObjectData> TestObjectSbtRecord;
