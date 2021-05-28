072c7afb05668f05379197e7407d378cf72bac5f
Blayze Millward <blayze.millward@googlemail.com>
2021-05-28 19:03:51 +0100

    Adds tiny OBJ Loader support files.

--- surfaceEyeGenerator/surfaceEyeGenerator.cpp
+++ surfaceEyeGenerator/surfaceEyeGenerator.cpp
@@ -1,3 +1,8 @@
+// Include tinyObjLoader
+//#define TINYOBJLOADER_USE_DOUBLE // Enable double-precision object loading
+#define TINYOBJLOADER_IMPLEMENTATION
+#include <support/tinyobjloader/tiny_obj_loader.h>
+
 #include <iostream>
 
 using namespace std;
@@ -6,7 +11,10 @@ int main(int argc, char** argv)
 {
   cout << "Starting surface eye generator..." << endl;
 
-  // Load the surface ( https://github.com/tinyobjloader/tinyobjloader )
+  // Load the surface using tiny OBJ loader ( https://github.com/tinyobjloader/tinyobjloader )
+  cout << "Loading surface..." << endl;
+  
+
   // Pre-calculate the surface's distances (also probably where the data that wraps the mesh is generated - such as neighbouring faces etc)
   // Open GL window? With tinyUI?
   // Place points
