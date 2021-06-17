62b7c20c88eb56affda0ee2dae43232bee588590
Blayzeing <blayze.millward@googlemail.com>
2021-06-17 18:31:23 +0100

    Adds additional eye helper functions relating to loading .eye files, makes the .eye definition more explicit.

--- python-examples/eyeRendererHelperFunctions.py
+++ python-examples/eyeRendererHelperFunctions.py
@@ -1,3 +1,4 @@
+import math
 from numpy.ctypeslib import ndpointer
 import numpy as np
 from ctypes import *
@@ -24,3 +25,30 @@ def setSamplesPerOmmatidium(eyeRenderer, samples):
   if(eyeRenderer.isCompoundEyeActive()):
     eyeRenderer.setCurrentEyeSamplesPerOmmatidium(samples)
     eyeRenderer.renderFrame()
+
+# Reads in a given eye file and returns it's information as an array of Ommatidium objects
+def readEyeFile(path):
+  output = []
+  with open(path) as eyeFile:
+    for line in eyeFile:
+      output.append(getEyeFeatures(line))
+  return output
+      
+class Ommatidium:
+  def __init__(self, position, direction, acceptanceAngle, focalpointOffset):
+    self.position = position
+    self.direction = direction
+    self.acceptanceAngle = acceptanceAngle
+    self.focalpointOffset = focalpointOffset
+
+  # Returns the solid angle, in steradians, of this ommatidium's cone of vision
+  def getSolidAngle(self):
+    return (2.0 * math.pi * (1.0-math.cos(self.acceptanceAngle/2.0)))
+
+def getEyeFeatures(line):
+  data = [float(n) for n in line.split(" ")]
+  position = np.asarray(data[:3])
+  direction = np.asarray(data[3:6])
+  acceptanceAngle = data[6]
+  focalPointOffset = data[7]
+  return (Ommatidium(position, direction, acceptanceAngle, focalPointOffset))
